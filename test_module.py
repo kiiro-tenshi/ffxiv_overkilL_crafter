0016d5e9e53a1778df7bd9244d1d27d5eb0036ea
Kiiro Tenshi <identityonlyforgaming@gmail.com>
2023-09-21 21:31:54 +0800

    Update test_module.py

--- test_module.py
+++ test_module.py
@@ -2,10 +2,11 @@ from modules import *
 from crafting_graph import *
 
 if __name__ == '__main__':
+    target_item = 'Baked Eggplant'
     #item_id = get_item_id('Ovibos Milk')
    #price_dict = fetch_item_price(item_id, 30)
    #print(get_recipe_id("not an item!"))
-    root = fetch_recipe_data(get_recipe_id("Baked Eggplant"), get_recipe_id, fetch_item_price, get_item_id)
+    root = fetch_recipe_data(get_recipe_id(target_item), get_recipe_id, fetch_item_price, get_item_id)
     Ingredient.print_tree(root)
     dictify = root.to_dict_quantity()
     dictify_price = root.to_dict_price()
@@ -13,6 +14,5 @@ if __name__ == '__main__':
     print(dictify)
     print("----")
     print(dictify_price)
-    target_item = 'Baked Eggplant'
     groceries_list = find_cheapest_way(dictify_price, target_item, verbose=True)
-    #get_item_name(35593)
+    # get_item_name(35593)
